906e835cedcbfb34324909b28178445182deadc2
Rosimeire Ribeiro <Rosimeire Ribeiro@DESKTOP-498FTIK>
2018-11-20 07:47:39 -0200

    Ajuste da logica

---
+++ AMEISA/Model/ProporcaoMutacionalDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace AMEISA.Model
+{
+    public class ProporcaoMutacionalDTO
+    {
+        public int NumNivelMutacao { get; set; }
+        public int QtdCombinacoes { get; set; }
+    }
+}
